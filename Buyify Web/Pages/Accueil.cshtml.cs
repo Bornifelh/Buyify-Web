cd2c9f930ec8ad1215748b94c59b7b24a9ecb30a
Derly MOUPEPIDI <moupepidid@gmail.com>
2023-05-25 00:01:15 +0100

    modifier-maintenantmodification

--- Buyify Web/Pages/Accueil.cshtml.cs
+++ Buyify Web/Pages/Accueil.cshtml.cs
@@ -65,4 +65,12 @@ namespace Buyify_Web.Pages
             _items.Add(item);
         }
     }
+
+    public class PanierItem
+    {
+        public int Id { get; set; }
+        public string? NomProduit { get; set; }
+        public double Prix { get; set; }
+        public int Quantite { get; set; }
+    }
 }
